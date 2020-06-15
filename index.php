9e814cb9f6bb0c927badf4a6df20d93e8594b4b3
Michael McPherson <michael@mpmcpherson.com>
2020-06-14 22:03:24 -0400

    filled out more file parse structure

--- index.php
+++ index.php
@@ -1,39 +1,101 @@
 <?php
 
 main();
-FileParser("");
-GetFiles("");
-FileImport("");
+//FileParser("");
+//GetFiles("");
+//FileImport("");
 
 function main()
 {
-	$fileList = GetFiles([])
+	$dataDirs = array();
+	array_push($dataDirs, './data');
+
+	//var_dump($dataDirs);
+	
+	$fileList = GetFiles($dataDirs);
+
+	//print_r($fileList);
+	$fileContents = array();
+	foreach($fileList as &$aryElem)
+	{
+		$singleFileContents = FileImport($aryElem);
+		array_push($fileContents, $singleFileContents);
+
+		//print_r($singleFileContents);
+	}
+	unset($aryElem);
+
+	//print_r($fileContents);
+
 	PrintCurrentFunction(__FUNCTION__);
 }
 
 function FileParser($file)
 {
+	$firstPass = explode("\n", $file);
+	$secondPass = array();
+	foreach($firstPass as &$fp)
+	{
+		/*
+		if (strpos($fp, 'ABRANCHES	19') !== false) {
+		    echo 'ABRANCHES	19'."\n";
+		}
+		*/
+		$explode = explode("\t", $fp);
+		$passBack = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $explode[0]); 
+		array_push($secondPass, $passBack);
+		/*
+		if (strpos($fp, 'ABRANCHES	19') !== false) {
+		    echo $passBack."\n";
+		}
+		*/
+	}
+	unset($fp);
+
+//	print_r($secondPass);
+
+	return $secondPass;
+
 	PrintCurrentFunction(__FUNCTION__);
 }
 
+//Get the data from the files on the list
 function FileImport($filePathAndName)
 {
+	$myfile = fopen($filePathAndName, "r") or die("Unable to open file!");
+	$newFile = fread($myfile,filesize($filePathAndName));
+	fclose($myfile);
+
+	//print_r($myfile);
+
+	$fileAry = FileParser($newFile);
+
+
+	return $fileAry;
+
 	PrintCurrentFunction(__FUNCTION__);
 }
 
+//Get the full file path of the files in the data directories.
 function GetFiles($ArrayOfDataDirectories)
-{
-	$outputDirectoryList = array();	
-	foreach($ArrayOfDataDirectories as $ary)
+{	
+
+	$outputFileList = array();
+
+	$fileList = scandir($ArrayOfDataDirectories[0]);
+
+	foreach($fileList as &$files)
 	{
-	//array push array and 
-		$fileList = scandir($ary);
-		var_dump($fileList);
-
+		if($files != "." && $files != "..")
+		{
+			array_push($outputFileList, $ArrayOfDataDirectories[0] . "/" . $files);
+		}
 	}
+	unset($files);
 
+	//PrintCurrentFunction(__FUNCTION__);
 
-	PrintCurrentFunction(__FUNCTION__);
+	return $outputFileList;
 }
 
 
@@ -42,7 +104,6 @@ function GetFiles($ArrayOfDataDirectories)
 function PrintCurrentFunction($funcName)
 {
 		echo $funcName . " working \n";
-
 }
 
 
